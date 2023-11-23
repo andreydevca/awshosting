b91b44a5249a4d3fe1d5695042676a1946f18c15
andreySoro <andrey@wewearmanyhats.com>
2023-11-23 13:51:26 -0700

    backend changes

--- src/components/main/Main.tsx
+++ src/components/main/Main.tsx
@@ -2,24 +2,54 @@
 import { FormEvent, useEffect, useState } from "react";
 
 type Props = {};
+type Todo = {
+  _id: string;
+  task: string;
+  createdAt: string;
+  updatedAt: string;
+  __v: number;
+}
 
 const Main = (props: Props) => {
-  const [todo, setTodo] = useState<string[]>([]);
+  const [todo, setTodo] = useState<Todo[]>([]);
   const [input, setInput] = useState<string>("");
 
-  const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
+  //FETCHING TODOS
+  useEffect(() => {
+    getTodos();
+  }, []);
+
+  const getTodos = async () => {
+    try {
+      const res = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/todo`);
+      const todos = await res.json();
+      setTodo(todos?.data);
+    } catch (error) {
+        console.log('error fetching todos')
+    }
+  };
+
+  //SUBMITTING NEW TODO
+  const handleSubmit = async (e: FormEvent<HTMLFormElement>) => {
     e.preventDefault();
-    if (!input.trim()) return;
-    setTodo([...todo, input]);
-    localStorage.setItem("todos", JSON.stringify([...todo, input]));
+    if (!input.trim()) return setInput("");
+    try {
+      const result = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/todo`, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ task: input }),
+      })
+      const data = await result.json()
+      const newTodos = [...todo, data?.data]
+      setTodo(newTodos);
+    } catch (error) {
+      console.log('error adding todo')
+    }
     setInput("");
   };
 
-  useEffect(() => {
-    const todos = localStorage.getItem("todos");
-    if (!todos) return;
-    setTodo(JSON.parse(todos));
-  }, []);
 
   return (
     <div>
@@ -45,12 +75,21 @@ const Main = (props: Props) => {
       <div className="mt-[2rem]">
         {todo.length > 0
           ? todo.map((item) => {
+            const date = new Date(item.createdAt)
+            const formattedDate = date.toLocaleDateString('en-US')
+            const time = date.toLocaleTimeString('en-US')
               return (
                 <div
                   className="bg-blue-100 p-4 rounded-md mb-2 max-w-[400px] flex-wrap"
-                  key={Math.ceil(Math.random() * 10000) + item}
+                  key={Math.ceil(Math.random() * 10000) + item?.task}
                 >
-                  <p className="font-[500] text-gray-600 break-all">{item}</p>
+                  <p className="font-[500] text-gray-600 break-all">{item.task}</p>
+                  <p className="text-xs text-gray-500 mt-1">
+                    Created at: {formattedDate}
+                  </p>
+                  <p className="text-xs text-gray-500 mt-1">
+                    {time}
+                  </p>
                 </div>
               );
             })
